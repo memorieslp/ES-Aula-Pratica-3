f10c5a677f9b02ceb997dbc662211bfe94820c02
MemoriesLP <raphaelrangel2005@yahoo.com.br>
2022-06-14 11:08:18 -0300

    first commit

---
+++ library.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class Book:
+    __slots__ = [
+        'name',
+        'author',
+    ]
+    
+    def __init__(self, name, author):
+        self.name = name
+        self.author = author
+
+def read_imput():
+    name = input()
+    author = input()
+    return name, author    
+        
+def main():
+    Book = read_imput()
+    
+if __name__ == '__main__':
+    main()
